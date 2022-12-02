f4548d9c05a39068a2d5aa36c5c98e06e7374d02
Martin Zhao <martinzha28@gmail.com>
2022-12-02 14:06:38 -0500

    Update Bishop.cc

---
+++ Bishop.cc
@@ -0,0 +1,17 @@
+coordToArray()
+
+void Piece::Bishop::move(string coords) {
+
+}
+
+bool Piece::Bishop::moveable(string coords) {
+    
+}
+
+std::vector<std::string> Piece::Bishop::squaresWatching() {
+
+}
+
+bool Piece::Bishop::underThreat() {
+
+}
