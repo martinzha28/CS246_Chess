e0142f77c3a705cc55be3b1519393c7612d9fba1
c9long <109810526+c9long@users.noreply.github.com>
2022-12-03 12:08:40 -0500

    adding black to board init

--- Board.cc
+++ Board.cc
@@ -8,19 +8,37 @@ void Board::init() {
                 if (j == 0) {
                     this.setupPiece('R', "a1");
                 } else if (j == 1) {
-                    this.setupPiece('N', "a2");
+                    this.setupPiece('N', "b1");
                 } else if (j == 2) {
-                    this.setupPiece('B', "a3");
+                    this.setupPiece('B', "c1");
                 } else if (j == 3) {
-                    this.setupPiece('Q', "a4");
+                    this.setupPiece('Q', "d1");
                 } else if (j == 4) {
-                    this.setupPiece('K', "a5");
+                    this.setupPiece('K', "e1");
                 } else if (j == 5) {
-                    this.setupPiece('B', "a6");
+                    this.setupPiece('B', "f1");
                 } else if (j == 6) {
-                    this.setupPiece('N', "a7");
+                    this.setupPiece('N', "g1");
                 } else if (j == 7) {
-                    this.setupPiece('R', "a8");
+                    this.setupPiece('R', "h1");
+                } 
+            } else if (i == 7) {
+                if (j == 0) {
+                    this.setupPiece('r', "a8");
+                } else if (j == 1) {
+                    this.setupPiece('n', "b8");
+                } else if (j == 2) {
+                    this.setupPiece('b', "c8");
+                } else if (j == 3) {
+                    this.setupPiece('q', "d8");
+                } else if (j == 4) {
+                    this.setupPiece('k', "e8");
+                } else if (j == 5) {
+                    this.setupPiece('b', "f8");
+                } else if (j == 6) {
+                    this.setupPiece('n', "g8");
+                } else if (j == 7) {
+                    this.setupPiece('r', "h8");
                 } 
             }
         }
